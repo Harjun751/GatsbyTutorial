244fb8056a367947d763e7dc03172b7055b816f4
unknown <Harjun751@gmail.com>
2023-05-06 09:11:02 +0800

    Gatsuuuubeh tutorial part 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,16 @@
 // Step 1: Importy React
 import * as React from 'react'
 import Layout from "../components/layout"
+import { StaticImage } from 'gatsby-plugin-image'
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby Tutorial.</p>
+      <StaticImage
+        alt="Two Wojacks pointing enthusiastically at something in the background (there is nothing there)."
+        src="../images/wojack.png"
+      />
     </Layout>
   )
 }
